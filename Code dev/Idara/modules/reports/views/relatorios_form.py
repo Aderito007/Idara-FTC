9dbf5039ed3bb31399f6d0945e23f550960663b7
Adérito <99333666+Aderito007@users.noreply.github.com>
2026-03-06 09:43:23 +0200

    Delete Code dev directory
    Actualizações dos arquivos

--- Code dev/Idara/modules/reports/views/relatorios_form.py
+++
@@ -1,8 +0,0 @@
-from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout
-
-class RelatoriosForm(QWidget):
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("Módulo Relatórios"))
-        self.setLayout(layout)
